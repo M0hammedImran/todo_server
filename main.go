d2aeb4cd103a6533e456c0580267ae4149090648
Mohammed Imran <mohammed.imran@protoezy.com>
2021-11-27 22:36:50 +0530

    minor changes
    Signed-off-by: Mohammed Imran <mohammed.imran@protoezy.com>

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	Completed bool
+	Id        int
+	Text      string
+}
+
+var todos = make([]Todo, 0)
+
 func main() {
-	os := os.Getenv("USERNAME")
-	
-	fmt.Printf("Hello world %s \n", os)
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		todo := append(todos, Todo{false, 1, "hello"})
+		copy(todos[1:], todos[1+1:])
+
+		// a[len(a)-1] = nil // or the zero value of T
+		// a = a[:len(a)-1]
+
+		return c.JSON(todo)
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
